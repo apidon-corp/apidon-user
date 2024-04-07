cc8f0d32f05fb4b790d8411786bcc25c38ba8e1c
aboveStars <yunuskorkmazis384@outlook.com>
2024-04-08 00:00:40 +0300

    Prepared for Mercury Launch.

--- src/hooks/providerHooks/useCheckProviderStatus.tsx
+++ src/hooks/providerHooks/useCheckProviderStatus.tsx
@@ -1,19 +1,15 @@
 import useGetFirebase from "../readHooks/useGetFirebase";
 
 export default function useCheckProviderStatus() {
-  const checkProviderStatusOnLogin = async (
-    username: string
-  ): Promise<
-    "server-error" | "no-current-provider" | "expired" | "good-to-go"
-  > => {
-    const { getDocServer } = useGetFirebase();
+  const { getDocServer } = useGetFirebase();
 
+  const checkProviderStatusOnLogin = async (username: string) => {
     const currentProviderDocResult = await getDocServer(
       `users/${username}/provider/currentProvider`
     );
     if (!currentProviderDocResult) {
       console.error("Error while getting current provider doc");
-      
+
       return "server-error";
     }
 
