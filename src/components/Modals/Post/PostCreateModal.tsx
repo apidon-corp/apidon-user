361c08d7c55c2e8028f40d0de5e5dded9cb417cb
aboveStars <yunuskorkmazis384@outlook.com>
2024-06-07 14:02:30 +0300

    Minor changes made.

--- src/components/Modals/Post/PostCreateModal.tsx
+++ src/components/Modals/Post/PostCreateModal.tsx
@@ -1,4 +1,3 @@
-import getCroppedImg from "@/components/utils/GetCroppedImage";
 import usePostUpload from "@/hooks/postHooks/usePostUpload";
 import {
   AspectRatio,
@@ -8,16 +7,15 @@ import {
   Input,
   Modal,
   ModalBody,
+  ModalCloseButton,
   ModalContent,
   ModalFooter,
+  ModalHeader,
   ModalOverlay,
-  Stack,
   Text,
   Textarea,
 } from "@chakra-ui/react";
-import { useCallback, useEffect, useRef, useState } from "react";
-import Cropper from "react-easy-crop";
-import { AiOutlineClose } from "react-icons/ai";
+import { useEffect, useRef, useState } from "react";
 import { BiImageAdd } from "react-icons/bi";
 import { useRecoilState } from "recoil";
 import { postCreateModalStateAtom } from "../../atoms/postCreateModalAtom";
@@ -25,24 +23,7 @@ import { postCreateModalStateAtom } from "../../atoms/postCreateModalAtom";
 export default function PostCreateModal() {
   const imageInputRef = useRef<HTMLInputElement>(null);
 
-  const [crop, setCrop] = useState({ x: 0, y: 0 });
-  const [zoom, setZoom] = useState(1);
-  const [croppedAreaPixels, setCroppedAreaPixels] = useState(null);
-
-  const onCropComplete = useCallback(
-    (croppedArea: any, croppedAreaPixels: any) => {
-      setCroppedAreaPixels(croppedAreaPixels);
-    },
-    []
-  );
-
-  const {
-    onSelectWillBeCroppedPhoto,
-    willBeCroppedPostPhoto,
-    setWillBeCroppedPostPhoto,
-    postUploadLoading,
-    sendPost,
-  } = usePostUpload();
+  const { uploadPost } = usePostUpload();
 
   const [postCreateModalState, setPostCreatModaleState] = useRecoilState(
     postCreateModalStateAtom
@@ -52,38 +33,12 @@ export default function PostCreateModal() {
     description: "",
     image: "",
   });
-
-  const [cropSize, setCropSize] = useState(400);
-  const cropRef = useRef<HTMLDivElement>(null);
-  const [minZoom, setMinZoom] = useState(1);
-
   const bigInputRef = useRef<HTMLTextAreaElement>(null);
   const smallInputRef = useRef<HTMLInputElement>(null);
 
   const [focusedTextInput, setFocusedInput] = useState<
     "bigInput" | "smallInput"
   >("smallInput");
-
-  useEffect(() => {
-    if (cropRef.current) setCropSize(cropRef.current.clientWidth);
-  }, [cropRef.current?.clientWidth]);
-
-  useEffect(() => {
-    const img = new Image();
-    img.onload = () => {
-      const width = img.width;
-      const height = img.height;
-      const ratio = width / height;
-
-      const abs = Math.abs(ratio - 1);
-
-      const finalZoomValue = abs * 2 + 1;
-
-      setZoom(finalZoomValue);
-      setMinZoom(finalZoomValue);
-    };
-    img.src = willBeCroppedPostPhoto;
-  }, [willBeCroppedPostPhoto]);
 
   const onTextsChanged = (
     event: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
@@ -99,13 +54,11 @@ export default function PostCreateModal() {
     if (!postCreateForm.description && !postCreateForm.image) {
       return console.log("You Can not create empty post, aborting");
     }
-    const operationResult = await sendPost(postCreateForm);
+    const operationResult = await uploadPost(postCreateForm);
 
     if (!operationResult) {
       return;
     }
-
-    handleResetCrop();
 
     setPostCreatModaleState((prev) => ({
       ...prev,
@@ -113,15 +66,6 @@ export default function PostCreateModal() {
     }));
 
     setPostCreateForm({ description: "", image: "" });
-  };
-
-  const handleResetCrop = () => {
-    setCrop({ x: 0, y: 0 });
-    setZoom(1);
-    setCroppedAreaPixels(null);
-    setWillBeCroppedPostPhoto("");
-
-    if (imageInputRef.current) imageInputRef.current.value = "";
   };
 
   useEffect(() => {
@@ -157,124 +101,36 @@ export default function PostCreateModal() {
       }}
       isOpen={postCreateModalState.isOpen}
       onClose={() => {
-        handleResetCrop();
-        if (!postUploadLoading)
-          setPostCreateForm({ description: "", image: "" });
+        setPostCreateForm({ description: "", image: "" });
         setPostCreatModaleState({ isOpen: false });
       }}
       autoFocus={false}
       scrollBehavior="inside"
     >
       <ModalOverlay backdropFilter="auto" backdropBlur="8px" />
-      <ModalContent bg="black">
-        <Flex
-          position="sticky"
-          top="0"
-          px={6}
-          align="center"
-          justify="space-between"
-          height="50px"
-          bg="black"
-        >
-          <Flex textColor="white" fontSize="17pt" fontWeight="700" gap={2}>
-            <Text>
-              {willBeCroppedPostPhoto ? "Adjust Your Photo" : "Create Post"}
-            </Text>
-          </Flex>
+      <ModalContent
+        bg="black"
+        minHeight={{
+          md: "500px",
+          lg: "500px",
+        }}
+      >
+        <ModalHeader color="white">Create Post</ModalHeader>
+        <ModalCloseButton color="white" />
 
-          <Icon
-            as={AiOutlineClose}
-            color="white"
-            fontSize="15pt"
-            cursor="pointer"
-            onClick={() => {
-              handleResetCrop();
-              if (!postUploadLoading)
-                setPostCreateForm({ description: "", image: "" });
-              setPostCreatModaleState({ isOpen: false });
-            }}
-          />
-        </Flex>
-
-        <ModalBody>
-          <Stack gap={1}>
-            <Text
-              as="b"
-              fontSize="14pt"
-              textColor="white"
-              hidden={!!willBeCroppedPostPhoto}
-            >
+        <ModalBody display="flex">
+          <Flex direction="column" width="100%" gap="5">
+            <Text as="b" fontSize="14pt" textColor="white">
               Photo
             </Text>
 
-            <Flex
-              id="post-image-crop-area"
-              direction="column"
-              hidden={!!!willBeCroppedPostPhoto}
-            >
-              <Flex id="try-cancel-buttons" justify="flex-end" gap={2} mb="2">
-                <Button
-                  size="sm"
-                  variant="solid"
-                  colorScheme="blue"
-                  onClick={async () => {
-                    // Get cropped image
-                    const croppedImage = await getCroppedImg(
-                      willBeCroppedPostPhoto,
-                      croppedAreaPixels
-                    );
-                    // Update State
-                    setPostCreateForm((prev) => ({
-                      ...prev,
-                      image: croppedImage as string,
-                    }));
-
-                    handleResetCrop();
-                  }}
-                >
-                  Try
-                </Button>
-                <Button
-                  size="sm"
-                  variant="outline"
-                  colorScheme="blue"
-                  onClick={() => {
-                    handleResetCrop();
-                  }}
-                >
-                  Cancel
-                </Button>
-              </Flex>
-              <Flex
-                position="relative"
-                height={cropSize}
-                width="100%"
-                mt={4}
-                ref={cropRef}
-              >
-                <Cropper
-                  image={willBeCroppedPostPhoto}
-                  crop={crop}
-                  zoom={zoom}
-                  aspect={1}
-                  onCropChange={setCrop}
-                  onCropComplete={onCropComplete}
-                  onZoomChange={setZoom}
-                  cropSize={{ height: cropSize, width: cropSize }}
-                  minZoom={minZoom}
-                  maxZoom={10}
-                />
-              </Flex>
-            </Flex>
-
-            <AspectRatio ratio={1} hidden={!!!postCreateForm.image}>
+            <AspectRatio ratio={1}>
               <img
                 style={{
                   borderRadius: "10px",
                 }}
                 alt=""
                 src={postCreateForm.image}
-                hidden={!!!postCreateForm.image}
               />
             </AspectRatio>
 
@@ -288,7 +144,6 @@ export default function PostCreateModal() {
               }}
               mt={2}
               colorScheme="red"
-              hidden={!!!postCreateForm.image}
             >
               Delete Photo
             </Button>
@@ -298,7 +153,6 @@ export default function PostCreateModal() {
               width="100%"
               borderRadius="4"
               bg="black"
-              hidden={!!willBeCroppedPostPhoto || !!postCreateForm.image}
               ratio={4 / 3}
             >
               <>
@@ -323,21 +177,14 @@ export default function PostCreateModal() {
                   ref={imageInputRef}
                   type="file"
                   accept="image/*"
-                  onChange={onSelectWillBeCroppedPhoto}
-                  isDisabled={postUploadLoading}
                   hidden
                 />
               </>
             </AspectRatio>
 
-            <Flex
-              direction="column"
-              mt={1}
-              gap="1"
-              hidden={!!willBeCroppedPostPhoto}
-            >
+            <Flex direction="column" mt={1} gap="1">
               <Text as="b" fontSize="14pt" textColor="white">
-                {postCreateForm.image ? "Description" : "Message"}
+                Description
               </Text>
               {postCreateForm.description.length >= 40 ? (
                 <Textarea
@@ -347,7 +194,6 @@ export default function PostCreateModal() {
                   fontWeight="600"
                   value={postCreateForm.description}
                   onChange={onTextsChanged}
-                  isDisabled={postUploadLoading}
                 />
               ) : (
                 <Input
@@ -357,14 +203,13 @@ export default function PostCreateModal() {
                   fontWeight="600"
                   value={postCreateForm.description}
                   onChange={onTextsChanged}
-                  isDisabled={postUploadLoading}
                 />
               )}
             </Flex>
-          </Stack>
+          </Flex>
         </ModalBody>
 
-        <ModalFooter hidden={!!willBeCroppedPostPhoto}>
+        <ModalFooter>
           <Button
             variant="outline"
             colorScheme="blue"
@@ -373,19 +218,10 @@ export default function PostCreateModal() {
               setPostCreatModaleState({ isOpen: false });
               setPostCreateForm({ description: "", image: "" });
             }}
-            isDisabled={postUploadLoading || !!willBeCroppedPostPhoto}
           >
             Cancel
           </Button>
-          <Button
-            colorScheme="blue"
-            onClick={handleSendPost}
-            isLoading={postUploadLoading}
-            isDisabled={
-              (!!!postCreateForm.description && !!!postCreateForm.image) ||
-              !!willBeCroppedPostPhoto
-            }
-          >
+          <Button colorScheme="blue" onClick={handleSendPost}>
             Post
           </Button>
         </ModalFooter>
--- src/hooks/postHooks/usePostUpload.tsx
+++ src/hooks/postHooks/usePostUpload.tsx
@@ -1,61 +1,25 @@
-import { currentUserStateAtom } from "@/components/atoms/currentUserAtom";
 import { PostCreateForm, PostServerDataV2 } from "@/components/types/Post";
 import { auth } from "@/firebase/clientApp";
-import { useRouter } from "next/router";
 import { useState } from "react";
-import { useRecoilValue } from "recoil";
 
 const usePostCreate = () => {
-  const [willBeCroppedPostPhoto, setWillBeCroppedPostPhoto] = useState("");
-  const [postUploadLoading, setPostUploadUpdating] = useState(false);
-  const currentUserstate = useRecoilValue(currentUserStateAtom);
-
-  const router = useRouter();
-
-  const onSelectWillBeCroppedPhoto = (
-    event: React.ChangeEvent<HTMLInputElement>
-  ) => {
-    if (!event.target.files) {
-      return console.error("No Files provided to onSelectWillBeCroppedPhoto");
-    }
-
-    const file = event.target.files[0];
-
-    if (!file.type.startsWith("image/")) {
-      return console.log("Only Images");
-    }
-
-    // 50mb
-    if (file.size > 50 * 10 ** 6) {
-      return console.error("This image is too high quality");
-    }
-
-    const reader = new FileReader();
-    reader.readAsDataURL(file);
-
-    reader.onload = (readerEvent) => {
-      setWillBeCroppedPostPhoto(readerEvent.target?.result as string);
-    };
-  };
-
   /**
    * @param postCreateForm
    * @returns true if operation is successfull, otherwise false.
    */
-  const sendPost = async (postCreateForm: PostCreateForm) => {
+  const uploadPost = async (postCreateForm: PostCreateForm) => {
     // This is my third control but, I don't trust states really :/
     if (!postCreateForm.description && !postCreateForm.image) {
       console.log("You Can not create empty post, aborting");
       return false;
     }
-    setPostUploadUpdating(true);
 
     let idToken = "";
     try {
       idToken = (await auth.currentUser?.getIdToken()) as string;
     } catch (error) {
       console.error("Error while post deleting. Couln't be got idToken", error);
-      setPostUploadUpdating(false);
+
       return false;
     }
 
@@ -74,7 +38,7 @@ const usePostCreate = () => {
       });
     } catch (error) {
       console.error("Error while fetching to 'postUpload' API", error);
-      setPostUploadUpdating(false);
+
       return false;
     }
 
@@ -83,42 +47,15 @@ const usePostCreate = () => {
         "Error while postUpload from 'postUpload' API",
         await response.text()
       );
-      setPostUploadUpdating(false);
+
       return false;
     }
 
-    const result = await response.json();
-    
-    const newPostServerData: PostServerDataV2 = result.newPostData;
-    const newPostDocId: string = result.newPostDocId;
-
-    // if (router.asPath === `/${currentUserstate.username}`) {
-    //   const newPostData: PostItemDataV2 = {
-    //     ...newPostServerData,
-    //     currentUserLikedThisPost: false,
-    //     id: newPostDocId,
-    //     currentUserFollowThisSender: false,
-    //   };
-    //   setPostsAtView((prev) => [newPostData, ...prev]);
-    // } else if (router.asPath === "/") {
-    //   const newPostData: PostItemDataV2 = {
-    //     ...newPostServerData,
-    //     currentUserLikedThisPost: false,
-    //     id: newPostDocId,
-    //     currentUserFollowThisSender: false,
-    //   };
-    //   setPostsAtView((prev) => [newPostData, ...prev]);
-    // }
-    setPostUploadUpdating(false);
     return true;
   };
 
   return {
-    willBeCroppedPostPhoto,
-    setWillBeCroppedPostPhoto,
-    onSelectWillBeCroppedPhoto,
-    sendPost,
-    postUploadLoading,
+    uploadPost,
   };
 };
 
